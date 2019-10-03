cb51bf478f8f4ba053a00074ba9ab6a653f9e6c8
tatanpoker09 <christian@eilers.cl>
2019-10-03 18:04:37 -0300

    Fixed a comment.

--- frameworklib/src/main/java/tatanpoker/com/frameworklib/framework/TreeDeviceManager.java
+++ frameworklib/src/main/java/tatanpoker/com/frameworklib/framework/TreeDeviceManager.java
@@ -4,6 +4,7 @@ package tatanpoker.com.frameworklib.framework;
 public abstract class TreeDeviceManager {
     /*
     TODO CREATE INSTANCES OF THE CAMERA AND ALARM (? I HAVE TO ANALYZE THIS.
+    POSSIBLE HAVE AN OWN GENERATED SUB-INIT WHERE ITS CUSTOM GENERATED AND IT INITIALIZES THEM.
      */
     public void init() {
 
