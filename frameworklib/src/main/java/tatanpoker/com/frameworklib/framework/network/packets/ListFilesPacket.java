d48755c2e0fcb6a3ae1e674026e2846ef2194e18
tatanpoker09 <christian@eilers.cl>
2019-11-30 04:32:08 -0300

    Finished request file list packet. Missing last part
    Signed-off-by: tatanpoker09 <christian@eilers.cl>

--- frameworklib/src/main/java/tatanpoker/com/frameworklib/framework/network/packets/ListFilesPacket.java
+++ frameworklib/src/main/java/tatanpoker/com/frameworklib/framework/network/packets/ListFilesPacket.java
@@ -1,16 +1,24 @@
 package tatanpoker.com.frameworklib.framework.network.packets;
 
+import java.io.File;
 import java.net.Socket;
 
 import tatanpoker.com.frameworklib.framework.network.ConnectionThread;
 import tatanpoker.com.frameworklib.framework.network.packets.types.SimplePacket;
 
 /*
- * TODO FINISH THIS PACKET.
+ * TODO FINISH THIS PACKET. KINDA LIKE REQUEST FROM NETWORK COMPONENT FILE LIST. THEN RETURN THIS.
  */
 public class ListFilesPacket extends SimplePacket {
-    public ListFilesPacket() {
+    private final int id_from;
+    private final int id_to;
+    private final File[] files;
+
+    public ListFilesPacket(int id_from, int id_to, File[] files) {
         super(EncryptionType.AES);
+        this.id_from = id_from;
+        this.id_to = id_to;
+        this.files = files;
     }
 
     @Override
--- frameworklib/src/main/java/tatanpoker/com/frameworklib/framework/network/packets/RequestFileListPacket.java
+++ frameworklib/src/main/java/tatanpoker/com/frameworklib/framework/network/packets/RequestFileListPacket.java
@@ -1,7 +1,10 @@
 package tatanpoker.com.frameworklib.framework.network.packets;
 
+import java.io.File;
 import java.net.Socket;
 
+import tatanpoker.com.frameworklib.exceptions.InvalidIDException;
+import tatanpoker.com.frameworklib.framework.Framework;
 import tatanpoker.com.frameworklib.framework.network.ConnectionThread;
 import tatanpoker.com.frameworklib.framework.network.packets.types.SimplePacket;
 
@@ -12,9 +15,13 @@ TODO FINISH THIS PACKET. CREATE PACKET SENDING A LIST WITH EVERYTHING BACK.
 public class RequestFileListPacket extends SimplePacket {
     private int id_from;
     private int id_to;
+    private String fileDir;
 
-    public RequestFileListPacket() {
+    public RequestFileListPacket(int id_from, int id_to, String fileDir) {
         super(EncryptionType.AES);
+        this.id_from = id_from;
+        this.id_to = id_to;
+        this.fileDir = fileDir;
     }
 
     @Override
@@ -24,6 +31,13 @@ public class RequestFileListPacket extends SimplePacket {
 
     @Override
     public void process(Socket socket, ConnectionThread clientThread) {
-
+        File directory = new File(fileDir);
+        File[] files = directory.listFiles();
+        ListFilesPacket listFilesPacket = new ListFilesPacket(id_to, id_from, files);
+        try {
+            Framework.getNetwork().sendPacket(id_from, listFilesPacket);
+        } catch (InvalidIDException e) {
+            e.printStackTrace();
+        }
     }
 }
